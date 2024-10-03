9b86dff2f33d576cb1a833fd42f2ad831c107e31
AlexanderContreras17 <hatakealexander24@gmail.com>
2024-10-02 18:00:06 -0600

    Cambios cache

--- PokemonPWA/wwwroot/serviceworker.js
+++ PokemonPWA/wwwroot/serviceworker.js
@@ -22,20 +22,74 @@ self.addEventListener("install", function (e) {
 self.addEventListener('fetch', event => {
     event.respondWith(getfromCache(event.request));
 });
-async function getfromCache(url) {
-    let cache = await caches.open(cacheName);
-    let response = await cache.match(url);
-    try {
-    if (response) {
-        return response;
-    }
-    else {
-        let respuesta = await fetch(url);
-        cache.put(url, respuesta.clone());
-        return respuesta;
-    }
-    }
-    catch (x) {
-        console.log(x);
-    }
-}
+//async function getfromCache(url) {
+//    let cache = await caches.open(cacheName);
+//    let response = await cache.match(url);
+//    try {
+//    if (response) {
+//        return response;
+//    }
+//    else {
+//        let respuesta = await fetch(url);
+//        cache.put(url, respuesta.clone());
+//        return respuesta;
+//    }
+//    }
+//    catch (x) {
+//        console.log(x);
+//    }
+//}
+
+////async function CacheOnly(url) {
+////    let cache = await caches.open(cacheName);
+////    let response = await cache.match(url);
+////    try {
+////        if (response) {
+////            return response;
+////        }
+////        else {
+////            return new response("no se encontro en cache");
+////        }
+////    }
+////    catch (x) {
+////        console.log(x);
+////    }
+////}
+
+//async function NetworkFirst(url) {
+//    let cache = await caches.open(cacheName);
+//    let response = await cache.match(url);
+//    try {
+//        let respuesta = await fetch(url);
+//        if (respuesta.ok) {
+//            cache.put(url, respuesta.clone());
+//            return respuesta;
+//        }
+       
+//    }
+//    catch (x) {
+//        let respuesta = await cache.match(url);
+//        if (response){
+        
+//            return response;
+//        }
+//        else
+//        console.log(x);
+//    }
+//}
+
+//async function StaleWhileRevalidate(url) {
+    
+//    try {
+//        let cache = await caches.open(cacheName);
+//        let response = await cache.match(url);
+//        let r= fetch(url).then(response => {
+//            cache.put(url, response.clone());
+//        });
+//        return response || r;
+
+//    }
+//    catch (x) {
+//            console.log(x);
+//    }
+//}
